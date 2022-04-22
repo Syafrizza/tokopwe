c3de1f4194d4f45a920e11ad14ca5c687b1f3709
Syafrizza <Syafrizza.bkl18@gmail.com>
2022-04-22 16:04:14 +0700

    menambahkan 10 routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
+// nama :Syafrizza Aulia Marizky
+// npm  : G1A020029
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SatuController;
+use App\Http\Controllers\DuaController;
+use App\Http\Controllers\TigaController;
+use App\Http\Controllers\EmpatController;
+use App\Http\Controllers\LimaController;
+use App\Http\Controllers\EnamController;
+use App\Http\Controllers\TujuhController;
+use App\Http\Controllers\DelapanController;
+use App\Http\Controllers\SembilanController;
+use App\Http\Controllers\SepuluhController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +25,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/beranda', function () {
+    echo ("Ini adalah Beranda");
 });
+
+Route::get('/profil', function () {
+    echo ("Syafrizza Aulia Marizky");
+});
+
+Route::get('/profil/syafrizza', function () {
+    echo ("Nama Syafrizza Aulia Marizky NPM G1020029");
+});
+
+Route::get('/awan', function () {
+    echo ("awan itu putih");
+});
+
+Route::get('/daun', function () {
+    echo ("Daun itu hijau");
+});
+
+Route::get('/profil/ayam', function () {
+    echo ("ayam itu berkokok ngk bisa terbang");
+});
+
+Route::get('/kuchengoyen', function () {
+    echo ("adalah ras terkuat");
+});
+
+Route::get('/payung', function () {
+    echo ("Hati-hati dengan payung");
+});
+
+Route::get('/alamat', function () {
+    echo ("Kapuas 4");
+});
+
+Route::get('/profil/hobi', function () {
+    echo (" Main Musik ");
+});
+
+//route controller
+Route::get('/Satu', [SatuController::class, 'Syafrizza']);
+Route::get('/Dua', [DuaController::class, 'Syafrizza']);
+Route::get('/Tiga', [TigaController::class, 'Syafrizza']);
+Route::get('/Empat', [EmpatController::class, 'Syafrizza']);
+Route::get('/Lima', [LimaController::class, 'Syafrizza']);
+Route::get('/Enam', [EnamController::class, 'Syafrizza']);
+Route::get('/Tujuh', [TujuhController::class, 'Syafrizza']);
+Route::get('/Delapan', [DelapanController::class, 'Syafrizza']);
+Route::get('/Sembilan', [SembilanController::class, 'Syafrizza']);
+Route::get('/Sepuluh', [SepuluhController::class, 'Syafrizza']);
